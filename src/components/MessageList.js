968dc228b4efc673656888e6877458b75bdc2c59
Brandon Rose <brandonrose805@gmail.com>
2020-09-24 21:56:56 -0700

    fix timestamp

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -12,6 +12,7 @@ class MessageList extends Component {
       roomId: '',
       username: '',
       sentAt: '',
+      renderedAt: Date.now()
     }
     this.messagesRef = this.props.firebase.database().ref('messages');
   }
@@ -23,16 +24,18 @@ class MessageList extends Component {
     this.messagesRef.on('child_added', snapshot => {
       const message = snapshot.val();
       message.key = snapshot.key;
-      this.setState({ messages: this.state.messages.concat(message) });
+      this.setState({
+        messages: this.state.messages.concat(message)
+      });
     });
-    this.scrollToBottom()
+    this.scrollToBottom();
   }
 
   componentDidUpdate() {
-    this.scrollToBottom()
+    this.scrollToBottom();
   }
 
-  handleChange(e){
+  handleChange(e) {
     this.setState({
       content: e.target.value,
       roomId: this.props.activeRoom,
@@ -43,7 +46,9 @@ class MessageList extends Component {
 
   handleSumbmit(e) {
     e.preventDefault();
-    if (!this.state.content) {return}
+    if (!this.state.content) {
+      return
+    }
     this.setState({
       content: '',
       roomId: '',
@@ -64,57 +69,86 @@ class MessageList extends Component {
   }
 
   deleteMessage(messageKey) {
-        const message = this.props.firebase.database().ref('/messages/' + messageKey);
-        message.remove();
-        const remainingMessages = this.state.messages.filter(message => message.key !== messageKey);
-        this.setState({ messages: remainingMessages});
-    }
-
-  scrollToBottom() {
-    this.messagesEndRef.current.scrollIntoView({ behavior: 'smooth' })
+    const message = this.props.firebase.database().ref('/messages/' + messageKey);
+    message.remove();
+    const remainingMessages = this.state.messages.filter(message => message.key !== messageKey);
+    this.setState({
+      messages: remainingMessages
+    });
   }
 
+  scrollToBottom() {
+    this.messagesEndRef.current.scrollIntoView({
+      behavior: 'smooth'
+    })
+  }
 
   render() {
-    const timeNow = <Moment format="MM/DD/YY"></Moment>;
     const roomMessages = this.state.messages.filter(message => message.roomId === this.props.activeRoom.key)
-                                            .map(message => {
+      .map(message => {
+        // 86400 -- one day in ms
+        // // formats timestamp
+        // let notToday = moment(message.sentAt).isBefore(timeNow);
+        // const sentToday = < Moment fromNow > {
+        //   message.sentAt
+        // } < /Moment>;
+        // const sentBefore = < Moment format = "HH:mm" > {
+        //   message.sentAt
+        // } < /Moment>;
+        //
+        // let timeSent;
+        // notToday ? timeSent = sentToday : timeSent = sentBefore;
 
-                                              // formats timestamp
-                                              let notToday = moment(message.sentAt).isBefore(timeNow);
-                                              const sentToday = <Moment fromNow>{ message.sentAt }</Moment>;
-                                              const sentBefore = <Moment format="HH:mm">{ message.sentAt }</Moment>;
-
-                                              let timeSent;
-                                              notToday ? timeSent = sentToday : timeSent = sentBefore;
-
-                                              return (
-                                                <div key={ message.key } className="message">
-                                                  <h4 className="username">{ message.username }: </h4>
-																									<button classname="delete-message transparent" onClick={() => this.deleteMessage(message.key)}>X</button>
-                                                  <p>{message.content}</p>
-                                                  <footer>
-                                                    <p>sent{notToday ? ":" : " at:"} {timeSent}</p>
-                                                  </footer>
-                                                </div>
-                                              )
-                                            });
+        return (
+          <div key = {message.key} className = "message" >
+            <h4 className = "username">
+              {message.username}:
+            </h4>
+            <button
+              classname = "delete-message transparent"
+              onClick = {() => this.deleteMessage(message.key)}>
+                X
+            </button>
+            <p>{message.content}</p>
+            <footer>
+              <p>sent: {
+                  // checks if more than 24 hours have passed and sets timestamp format
+                  // 8640000 is 24 hours in milliseconds
+                  ((this.state.renderedAt - message.sentAt) > 8640000)
+                  ?
+                    <Moment format="MM/DD/YYYY">{message.sentAt}</Moment>
+                  :
+                    <Moment fromNow>{message.sentAt}</Moment>
+                }
+              </p>
+            </footer>
+          </div>
+        );
+      });
 
     return (
-      <div className="message-list">
-        <h1 className="room-name">#{this.props.activeRoom.name.toLowerCase()}</h1>
-        <div className="messages">{roomMessages}
-          <div ref={this.messagesEndRef} />
-        </div>
-        <form className="create-new-message" onSubmit={ (e) => this.handleSumbmit(e) }>
+      <div className = "message-list">
+        <h1 className = "room-name">
+          #{this.props.activeRoom.name.toLowerCase()}
+        </h1>
+        <div className = "messages">
+          {roomMessages}
+        <div ref={this.messagesEndRef}/></div>
+        <form
+          className = "create-new-message"
+          onSubmit={(e) => this.handleSumbmit(e)}>
           <input
-            type="text"
-            placeholder="say something interesting..."
-            value={this.state.content}
-            onChange={ (e) => this.handleChange(e) }
-						className="new-message room-input"
+            type = "text"
+            placeholder = "say something interesting..."
+            value = {this.state.content}
+            onChange = {(e) => this.handleChange(e)}
+            className = "new-message room-input"
           />
-          <button className="transparent" type="sumbit">Send</button>
+          <button
+            className = "transparent"
+            type = "sumbit">
+              Send
+          </button>
         </form>
       </div>
     )
